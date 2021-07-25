ee4d24c4261a5ded6857da589b07c86ca73f2d57
thomas <thomas800117@hotmail.com>
2021-07-25 22:39:08 +0800

    '建立投票'

--- app.py
+++ app.py
@@ -15,6 +15,8 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('9yposcY6LT8+69GqLz7+9PoCcvqW/PalD/z8qGdwqu3PqwN7/7FIOmD1WNP0rdR4PRvuUUrhD7ZG+ocPL4KXNl+qRtZeJqZFYeYzFhpq+hKPPk/YR55ewU4a5ssZpTPJVy2TLl3pLt7e6mGddfZXqAdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('abea3b590abf8bb6fa7934a6682a7a7e')
 
+votes = []
+
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -37,6 +39,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    global votes
     msg = event.message.text
     response = '我還聽不懂這句話'
 
@@ -44,13 +47,24 @@ def handle_message(event):
         response = '你好啊,87'
     elif msg in ['天竺鼠車車']:
         response = '天竺鼠車車好看哦 快來看: https://www.youtube.com/watch?v=_6TtTRrno3E'
-    if msg in ['!投票']:
-        response = '測試看看'
+    if msg in ['!建立投票']:
+        add_votes(msg)
+        response = votes
+        # vote = msg.find('!建立投票');
+        # response = '測試看看'
+
 
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=response))
 
+def add_votes(str):
+	global votes
+	if(str.find('!建立投票') == 0):
+		tmp = str.split(' ')
+		tmp.pop(0)
+		# votes = []
+		votes+= tmp
 
 if __name__ == "__main__":
     app.run()
